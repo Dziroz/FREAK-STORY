1d6dbce9e5c3f83adba9d26086a313c8a6c6bbf8
Dziroz <abcdanil@bk.ru>
2023-04-09 01:53:51 +0500

    ТЕКСТУРЫ!!!!!!!!!!!!!!!!!!!!!!!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     private Vector3 change;
+    private Animator animator;
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -18,12 +20,34 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetKeyDown(KeyCode.Space) && animator.GetBool("attacking") == false)
+        {
+            StartCoroutine(AttackCo());
+        }
     }
     private void FixedUpdate()
+    {
+        UpdateAnimationAndMove();
+    }
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.05f);
+    }
+    void UpdateAnimationAndMove() 
     {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
     void MoveCharacter()
--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -8,7 +8,7 @@ public class Sign : MonoBehaviour
     TextMeshProUGUI dialogText;
     public GameObject dialogBox;
     public string dialog;
-    public bool dialogActive;
+    public bool playerInRange;
     void Start()
     {
         
@@ -17,20 +17,32 @@ public class Sign : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
        if(other.tag == "Player")
         {
-
+            playerInRange = true;
         } 
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-
+            playerInRange = false;
+            dialogBox.SetActive(false);
         }
     }
 }
