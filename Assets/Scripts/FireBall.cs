890e3cb2e2e46ad12f1991d702c6967464ac9e64
Dziroz <abcdanil@bk.ru>
2023-04-12 02:50:48 +0500

    ХУЕТА

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject point;
+    public Vector2 lastPosition;
+    public float speed;
+    public bool isPoint =true;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //lastPosition = player.transform;
+
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        if (isPoint)
+        {
+            lastPosition = player.transform.position;
+            isPoint = false;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, lastPosition, 0.1f);
+    }
+}
